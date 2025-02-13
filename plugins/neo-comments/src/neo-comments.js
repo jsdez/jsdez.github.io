47d47b878a4f9bede3fe32ec63fb726dd0f0b525
Jake Dennison <jake.dennison@hotmail.co.uk>
2025-02-13 12:46:52 +0000

    .

--- plugins/neo-comments/src/neo-comments.js
+++ plugins/neo-comments/src/neo-comments.js
@@ -288,11 +288,23 @@ class CommentsElement extends LitElement {
           placeholder="Write your comment here..."
         ></textarea>
         <button
-          class="btn btn-default"
+          class="btn btn-default d-flex align-items-center"
           type="button"
           @click=${this.addComment}
           ?disabled=${!this.newComment.trim()}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-send me-2"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"
+            />
+          </svg>
           Add Comment
         </button>
       </div>
