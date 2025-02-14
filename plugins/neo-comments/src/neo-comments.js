d417b368eda9ba471251fdc555268b4ec72ee7eb
Jake Dennison <jake.dennison@hotmail.co.uk>
2025-02-14 09:39:00 +0000

    .

--- plugins/neo-comments/src/neo-comments.js
+++ plugins/neo-comments/src/neo-comments.js
@@ -257,6 +257,10 @@ class CommentsElement extends LitElement {
     this.newComment = '';
   }
 
+  handleCommentChange(e) {
+    this.newComment = e.target.value;
+  }
+
   render() {
     return html`
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
