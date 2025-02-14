3da4334959ce59f612a1d8e1f732b9dd1347c88c
Jake Dennison <jake.dennison@hotmail.co.uk>
2025-02-14 15:59:47 +0000

    .

--- plugins/neo-comments/src/neo-comments.js
+++ plugins/neo-comments/src/neo-comments.js
@@ -47,7 +47,7 @@ class CommentsElement extends LitElement {
           isValueField: true,
           properties: {
             Comments: {
-              type: 'object',
+              type: 'array',
               description: 'Array of comments',
               items: {
                 type: 'object',
