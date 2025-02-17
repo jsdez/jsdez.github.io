b08c319d670374b6cfc98465fd9dda80913dd1de
Jake Dennison <jake.dennison@hotmail.co.uk>
2025-02-17 12:27:12 +0000

    .

--- plugins/neo-comments/src/neo-comments.js
+++ plugins/neo-comments/src/neo-comments.js
@@ -35,11 +35,7 @@ class CommentsElement extends LitElement {
           ],
           defaultValue: 'Default',
         },
-        inputobj: {
-          type: 'object',
-          title: 'Input Object',
-          description: 'Enter the comments object from previous control here',
-        },
+        email: { type: 'string', title: 'Email Address' },
         outputobj: {
           title: 'Comments Output',
           type: 'object',
@@ -79,7 +75,7 @@ class CommentsElement extends LitElement {
         },
       },
       events: ['ntx-value-change'],
-      standardProperties: { fieldLabel: true, description: true, readOnly: true, visibility: true },
+      standardProperties: { fieldLabel: true, description: true, readOnly: true, visibility: true, placeholder: true, defaultValue: true },
     };
   }
 
@@ -89,10 +85,12 @@ class CommentsElement extends LitElement {
     email: { type: String },
     badge: { type: String },
     badgeStyle: { type: String },
-    inputobj: { type: Object },
+    defaultValue: { type: Object },
     workingComments: { type: Array },
     newComment: { type: String },
     readOnly: { type: Boolean },
+    placeholder: { type: String},
+    defaultValue: { type: Object},
     deletableIndices: { type: Array },
   };
 
@@ -103,16 +101,16 @@ class CommentsElement extends LitElement {
     this.email = '';
     this.badge = 'Update';  // Default Badge
     this.badgeStyle = 'Default';  // Default Badge Style
-    this.inputobj = null;
+    this.defaultValue = null;
     this.workingComments = [];
     this.newComment = '';
     this.deletableIndices = [];
   }
 
   updated(changedProperties) {
-    if (changedProperties.has('inputobj') && Array.isArray(this.inputobj?.comments)) {
-      this.workingComments = [...this.inputobj.comments];
-      this.deletableIndices = []; // Reset deletable indices when inputobj changes
+    if (changedProperties.has('defaultValue') && Array.isArray(this.defaultValue?.comments)) {
+      this.workingComments = [...this.defaultValue.comments];
+      this.deletableIndices = []; // Reset deletable indices when defaultValue changes
     }
 
     if (changedProperties.has('readOnly')) {
